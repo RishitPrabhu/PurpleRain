f058afdce3d7fa9d6c91c7561e119899a4ceabbc
RishitP <rishitprabhu.personal@gmail.com>
2025-08-15 01:09:59 +0530

    Added Random Speed

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,25 @@
 #include "raylib.h"
-#include "raymath.h"
-#include <list>
-
 
 int main() {
 
     int screenWidth = 800;
     int screenHeight = 350;
+    int count = 200;
+    int counter = 0;
 
     Vector2 pos = {(float)screenWidth/2, 0};
     int speedY = 2;
-    std::list<float> posYList = {};
+    int posXList[count];
+    int posYList[count];
+
     for(int i = 0; i < 100; i++){
-      posYList.push_back(GetRandomValue(0, screenWidth));
-    } 
+      posXList[i] = (GetRandomValue(0, screenWidth));
+    }
+
+    int speedYList[100];
+    for(int i = 0; i < 100; i++){
+      speedYList[i] = (GetRandomValue(1500, 2500));
+    }
 
     InitWindow(screenWidth, screenHeight, "Purple Rain");
     SetTargetFPS(120);
@@ -21,11 +27,26 @@ int main() {
     while (!WindowShouldClose()) {   
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            for(int i = 0; i < 100; i++){
-              pos.x =  GetRandomValue(0, screenWidth); 
-              DrawRectangle(pos.x, pos.y, 1, 15, BLACK);
+            for(int i = 0; i < count; i++){
+              posYList[i] += speedYList[i]/1000;
+              
+              DrawRectangle(posXList[i], posYList[i], 1, 15, BLACK);
             }
             pos.y += (float)speedY;
+
+            for (int i = 0; i < count; i++)
+            {
+              if(posYList[i] <= screenHeight)
+                counter++;
+            }
+
+            if(counter < 75){
+              for(int i = 0; i < count; i++){
+              posYList[i] += speedYList[i]/1000;
+              
+              DrawRectangle(posXList[i], posYList[i], 1, 15, BLACK);
+            }
+            }
         EndDrawing();
     }
 
